4bf797474875034f1c51aa72956fc8a93e083cf3
Zack-hc <zhaci001@gmail.com>
2023-08-13 06:09:29 -0700

    printing by us

--- models/base_model.py
+++ models/base_model.py
@@ -10,17 +10,16 @@ class BaseModel:
 
     def __init__(self, my_number=None, name=None):
         """init"""
-
         self.my_number = my_number
         self.name = name
+        self.updated_at = datetime.now()
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
     def __str__(self):
         """ instance strrr"""
 
-        return f"[{self.name}] ({self.id}) {self.__dict__}"
+        return f"[BaseModel] ({self.id}) {self.__dict__}"
 
     def save(self):
         """ instance save"""
@@ -31,8 +30,8 @@ class BaseModel:
         """ instance dic"""
 
         delta = self.__dict__.copy()
-        delta['__class__'] = self.name
-        delta['created_at'] = self.created_at.isoformat()
+        delta['__class__'] = type(self).__name__
         delta['updated_at'] = self.updated_at.isoformat()
-        self.__dict__
+        delta['id'] = self.id
+        delta['created_at'] = self.created_at.isoformat()
         return delta
