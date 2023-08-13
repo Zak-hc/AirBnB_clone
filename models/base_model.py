b2501396cbe68c2dfe579e12dd0faaf60a82c3f9
Zack-hc <zhaci001@gmail.com>
2023-08-13 06:42:58 -0700

    printing by us

--- models/base_model.py
+++ models/base_model.py
@@ -19,6 +19,7 @@ class BaseModel:
         self.updated_at = datetime.now()
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
+
     def __str__(self):
         """ instance strrr"""
 
