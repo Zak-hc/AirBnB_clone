174c255b28536949bbf913c4aa8a9a37ce2e4592
Zack-hc <zhaci001@gmail.com>
2023-08-13 06:40:47 -0700

    printing by us

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,15 @@ class BaseModel:
 
     def __init__(self, my_number=None, name=None):
         """init"""
-        self.my_number = my_number
-        self.name = name
+        if name is not None and my_number is not None:
+            self.my_number = my_number
+            self.name = name
+        else:
+            self.my_number = None
+            self.name = None
         self.updated_at = datetime.now()
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-
     def __str__(self):
         """ instance strrr"""
 
@@ -31,7 +34,10 @@ class BaseModel:
 
         delta = self.__dict__.copy()
         delta['__class__'] = type(self).__name__
-        delta['updated_at'] = self.updated_at.isoformat()
-        delta['id'] = self.id
-        delta['created_at'] = self.created_at.isoformat()
+        if self.created_at is not None:
+            delta['created_at'] = self.created_at.isoformat()
+        if self.updated_at is not None:
+            delta['updated_at'] = self.updated_at.isoformat()
+        if self.id is not None:
+            delta['id'] = self.id
         return delta
